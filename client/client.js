d935fadaee9c7c3eae9d0798fbd5176eca03f567
Kristjan-S <kristjansesek@gmail.com>
2014-09-04 18:51:29 +0200

    Migration to Meteor 0.9

--- client/client.js
+++ client/client.js
@@ -1,27 +1,34 @@
+CurrentTabs = new Meteor.Collection(null);
+
 // client code: ping heartbeat every second for immidiate effect in current users
 Meteor.setInterval(function () {
-	Meteor.call('keepalive', Session.get('current_user'));
+	Meteor.call('keepalive', Session.get('currentUser'));
 }, 1000);
 
 
 var connectHandler = WebApp.connectHandlers; 
 
 Meteor.startup(function() {
-	if(Session.get('current_page')==undefined)
-		Session.setTemporary('current_page',1);
+	if(Session.get('currentPage')==undefined)
+		Session.setTemporary('currentPage',1);
+	if(Session.get('currentTabs')==undefined) {
+		var currentTabs = [];
+		Session.set('currentTabs',currentTabs);
+	}
 	
-	Meteor.subscribe('current_groups');
-	Meteor.subscribe('current_connections', function() {
-		if(Session.get('current_user')==undefined)
+
+	Meteor.subscribe('currentGroups');
+	Meteor.subscribe('currentConnections', function() {
+		if(Session.get('currentUser')==undefined)
 		{
-			var set=1;
+			var set = true;
 			while(set)
 			{
 				var tmp = Math.floor(Math.random()*1000+1);
-				if(Connections.find({"user_id": tmp}).count()==0)
+				if(Connections.find({"userId": tmp}).count()==0)
 				{
-					Session.set('current_user',tmp)
-					set=0;
+					Session.set('currentUser',tmp);
+					set = false;
 				}
 			}
 
--- server/server.js
+++ server/server.js
@@ -2,25 +2,24 @@
 Server code
 **/
 
-
-Meteor.publish('current_connections', function publishConnections() {
+Meteor.publish('currentConnections', function publishConnections() {
   return Connections.find({});
 });
 
-Meteor.publish('current_groups', function publishGroups() {
+Meteor.publish('currentGroups', function publishGroups() {
   return Groups.find({});
 });
 
 Meteor.methods({
   keepalive: function (userId) {
-    if (Connections.find({'user_id': userId}).count()==0)
-      Connections.insert({'user_id': userId, 'last_seen': (new Date()).getTime()});
+    if (Connections.find({'userId': userId}).count()==0)
+      Connections.insert({'userId': userId, 'lastSeen': (new Date()).getTime()});
     else
-    	Connections.update({'user_id': userId}, {$set: {'last_seen': (new Date()).getTime()}}, {upsert: false});
+    	Connections.update({'userId': userId}, {$set: {'lastSeen': (new Date()).getTime()}}, {upsert: false});
   }
 });
 
 Meteor.setInterval(function () {
   var now = (new Date()).getTime();
-  Connections.remove({last_seen: {$lt: (now - 1 * 1000)}});
+  Connections.remove({'lastSeen': {$lt: (now - 1 * 5000)}});
 }, 1000);
