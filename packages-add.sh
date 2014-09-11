e06c07f1749ce4e158eb9a6b6f8ee9c7adbfc72b
Kristjan-S <kristjansesek@gmail.com>
2014-09-12 00:50:14 +0200

    Tabs implemented.

---
+++ packages-add.sh
@@ -0,0 +1,15 @@
+# Meteor packages used by this project, one per line.
+#
+# 'meteor add' and 'meteor remove' will edit this file for you,
+# but you can also edit it by hand.
+
+meteor add standard-app-packages
+meteor add insecure
+meteor add cmather:iron-router
+meteor add u2622:persistent-session
+meteor add chrismbeckett:fontawesome4
+meteor add accounts-facebook
+meteor add accounts-google
+meteor add accounts-twitter
+meteor add accounts-base
+meteor add accounts-ui
